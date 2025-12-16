3338232f6e1745208015413ed464e0acb4cf2efb
danylo-gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2025-12-16 19:51:12 +0200

    chore: add SnapKit

---
+++ Sources/Views/Admin/AdminLessonCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+import SnapKit
+
+final class AdminLessonCell: UITableViewCell {
+    
+}
---
+++ Sources/Views/Admin/AdminScheduleViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AdminScheduleViewController.swift
+//  university-schedule-app
+//
+//  Created by TataAndreas on 16.12.2025.
+//
+
+import Foundation
